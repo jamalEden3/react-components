e36284f362816586d921662164310c9c524e451f
jamaleden <jamaleden6@gmail.com>
2022-09-08 14:02:41 +0200

    add validation

--- src/pages/FormPage/FormPage.js
+++ src/pages/FormPage/FormPage.js
@@ -1,11 +1,65 @@
-import * as React from 'react';
+import {react, useState} from 'react';
+
+const INITIAL_STATE = {
+  email: '',
+  password: '',
+};
+
+const VALIDATION = {
+  email: [
+    {
+      isValid: (value) => !!value,
+      message: 'Is required.',
+    },
+    {
+      isValid: (value) => /\S+@\S+\.\S+/.test(value),
+      message: 'Needs to be an email.',
+    },
+  ],
+  password: [
+    {
+      isValid: (value) => !!value,
+      message: 'Is required.',
+    },
+  ]
+}
+
+const getErrorFields = (form) =>
+  // get keys of form => [email, password]
+  Object.keys(form).reduce((acc, key) => {
+    // if VALIDATION has not the key provided return acc
+    if(!VALIDATION[key]) return acc;
+
+    // get errors for every key [email and password]
+    const errorsPerField = VALIDATION[key]
+      .map((validation) => ({
+        isValid: validation.isValid(form[key]),
+        message: validation.message
+      })).filter((errorPerField) => !errorPerField.isValid);
+      return { ...acc, [key]: errorsPerField }
+  }, {})
+
+
+const getDirtyFields = (form) => {
+  const ff = Object.keys(form).reduce((acc, key) => {
+    const isDirty = form[key] !== INITIAL_STATE[key];
+
+    return { ...acc, [key]: isDirty };
+  }, {});
+  return ff;
+}
 
 const FormPage = () => {
-  const INITIAL_STATE = {
-    email: '',
-    password: '',
-  };
-  const [form, setForm] = React.useState(INITIAL_STATE);
+  const [form, setForm] = useState(INITIAL_STATE);
+  
+  const dirtyFields = getDirtyFields(form);
+  const errorsFields = getErrorFields(form);
+  
+console.log(errorsFields)
+  const hasChanges = Object.values(dirtyFields).every(
+    // [true, false]
+    (value) => value
+  );
 
   const handleChange = (event) => {
     setForm({
@@ -13,11 +67,13 @@ const FormPage = () => {
       [event.target.id]: event.target.value,
     });
   };
-
+  
   const handleSubmit = (event) => {
     event.preventDefault();
+    const hasError = Object.values(errorsFields).flat().length;
+    if(hasError) return;
     setForm(INITIAL_STATE);
-    alert(form.email + ' ' + form.password);
+    alert(form.email + form.password)
   };
 
   return (
@@ -30,6 +86,13 @@ const FormPage = () => {
           value={form.email}
           onChange={handleChange}
         />
+        {
+          errorsFields.email?.length? (
+            <span style={{color: 'red'}}>
+              {errorsFields.email[0].message}
+            </span>
+          ) : null
+        }
       </div>
       <div>
         <label htmlFor="password">Password</label>
@@ -39,8 +102,15 @@ const FormPage = () => {
           value={form.password}
           onChange={handleChange}
         />
+        {
+          errorsFields.password?.length? (
+            <span style={{color: 'red'}}>
+              {errorsFields.password[0].message}
+            </span>
+          ) : null
+        }
       </div>
-      <button type="submit">Submit</button>
+      <button disabled={!hasChanges} type="submit">Submit</button>
     </form>
   );
 };
